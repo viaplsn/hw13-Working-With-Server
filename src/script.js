6a73325c3e61dd577726d7c237a0a98639fbd7c6
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-03-31 16:08:35 +0300

    get info about characters button and css styles added.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, World!')
+const getInfoButton = document.getElementById('info-button');
+getInfoButton.addEventListener('click', getCharInfo);
+
+const infoContainer = document.getElementById('info-container');
+
+function getCharInfo() {
+    axios.get('https://swapi.dev/api/films/5/')
+    .then((result) => {
+        const charLinks = result.data.characters
+        charLinks.forEach(element => {
+            axios.get(element)
+                .then((result) => {
+                    const name = result.data.name;
+                    const birth = result.data.birth_year;
+                    const gender = result.data.gender;
+                    if(gender === "male") {
+                        infoContainer.insertAdjacentHTML("beforeend", `<div class="character-container"><h3>${name}</h3><p class="char-info">Birth date: ${birth}</p><object class="gender" type="image/svg+xml" data="assets/images/man_icon.svg"></object></div>`);
+                    } else if(gender === "female") {
+                        infoContainer.insertAdjacentHTML("beforeend", `<div class="character-container"><h3>${name}</h3><p class="char-info">Birth date: ${birth}</p><object class="gender" type="image/svg+xml" data="assets/images/woman_icon.svg"></object></div>`);
+                    } else {
+                        infoContainer.insertAdjacentHTML("beforeend", `<div class="character-container"><h3>${name}</h3><p class="char-info">Birth date: ${birth}</p><object class="gender" type="image/svg+xml" data="assets/images/creature_icon.svg"></object></div>`);
+                    }
+                    getInfoButton.removeEventListener('click', getCharInfo);
+                })
+        });
+    });
+};
